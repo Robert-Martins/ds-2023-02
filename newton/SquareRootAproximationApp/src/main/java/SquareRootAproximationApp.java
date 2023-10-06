953c3c2d106282dadd2dda6275c86674df0e2665
Robert Cardoso Cantares Martins <78915072+Robert-Martins@users.noreply.github.com>
2023-10-06 20:40:58 -0300

    Update SquareRootAproximationApp.java

--- newton/SquareRootAproximationApp/src/main/java/SquareRootAproximationApp.java
+++ newton/SquareRootAproximationApp/src/main/java/SquareRootAproximationApp.java
@@ -5,8 +5,8 @@ public class SquareRootAproximationApp {
     public static void main(String[] args) {
         System.out.println("Insira um valor:\n");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Resultado: " + SquareRootAproximation.calculateSquareRoot(scanner.nextInt(), 0.001, 100));
-        System.out.println("Obtido com Epsilon de 0.001 e um Máximo de 100 iterações");
+        System.out.println("Resultado: " + SquareRootAproximation.calculateSquareRoot(scanner.nextInt());
+        System.out.println("Obtido com Epsilon de 1e-10");
     }
 
 }
