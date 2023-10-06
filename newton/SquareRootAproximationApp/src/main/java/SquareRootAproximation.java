6e4687616b40057c8bd10750b5162955e5d01826
Robert Cardoso Cantares Martins <78915072+Robert-Martins@users.noreply.github.com>
2023-10-06 20:40:32 -0300

    Update SquareRootAproximation.java

--- newton/SquareRootAproximationApp/src/main/java/SquareRootAproximation.java
+++ newton/SquareRootAproximationApp/src/main/java/SquareRootAproximation.java
@@ -1,16 +1,15 @@
 public class SquareRootAproximation {
 
-    public static double calculateSquareRoot(Integer x, Double epsilon, Integer maxIterations) {
+    private static final Double EPSILON = 1e-10;
+
+    public static double calculateSquareRoot(Integer x) {
         if (x < 0)
             throw new IllegalArgumentException("X não pode ser negativo");
-
+        
         Double guess = x / 2.0;
-        Integer iterations = 0;
 
-        while (Math.abs(guess * guess - x) > epsilon && iterations < maxIterations) {
+        while(Math.abs(guess * guess - x) > EPSILON)
             guess = 0.5 * (guess + x / guess);
-            iterations++;
-        }
 
         return guess;
     }
