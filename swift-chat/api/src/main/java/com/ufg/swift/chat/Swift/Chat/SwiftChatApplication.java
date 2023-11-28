3f4a754e4da4a6795da33a4f865b930a308882b5
Robert-Martins <robertcantares@gmail.com>
2023-11-28 19:39:48 -0300

    inicio-projeto-final

---
+++ swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/SwiftChatApplication.java
@@ -0,0 +1,13 @@
+package com.ufg.swift.chat.Swift.Chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftChatApplication.class, args);
+	}
+
+}
---
+++ swift-chat/api/src/test/java/com/ufg/swift/chat/Swift/Chat/SwiftChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufg.swift.chat.Swift.Chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
