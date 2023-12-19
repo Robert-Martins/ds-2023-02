e35f9803717e3cee9116f4f74bbc03f653dc10b8
Robert-Martins <robertcantares@gmail.com>
2023-12-19 19:10:31 -0300

    feat/configs

--- swift-chat/api/src/test/java/com/ufg/swift/chat/Swift/Chat/SwiftChatApplicationTestsRoom.java
+++ swift-chat/api/src/test/java/com/ufg/swift/chat/Swift/Chat/SwiftChatApplicationTestsRoom.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SwiftChatApplicationTests {
+class SwiftChatApplicationTestsRoom {
 
 	@Test
 	void contextLoads() {
