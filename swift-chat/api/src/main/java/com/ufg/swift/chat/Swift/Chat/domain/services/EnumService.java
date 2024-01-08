8d91d3b191051bee5ba1a389052a5a42b89b5d66
Robert-Martins <robertcantares@gmail.com>
2024-01-08 20:40:17 -0300

    feat/include-enum-loading-option

--- swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/domain/services/EnumService.java
+++ swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/domain/services/EnumService.java
@@ -1,5 +1,6 @@
 package com.ufg.swift.chat.Swift.Chat.domain.services;
 
+import com.ufg.swift.chat.Swift.Chat.infrastructure.vo.enums.ChatEventType;
 import com.ufg.swift.chat.Swift.Chat.infrastructure.vo.enums.UserRoleInChat;
 import com.ufg.swift.chat.Swift.Chat.presentation.dtos.EnumDto;
 import com.ufg.swift.chat.Swift.Chat.presentation.services.IEnumService;
@@ -15,6 +16,7 @@ public class EnumService implements IEnumService {
     public List<EnumDto> loadEnumByType(String type) {
         return switch (type) {
             case "userRoleInChat" -> Arrays.stream(UserRoleInChat.values()).map(e -> new EnumDto(e.name(), e.getDescription())).toList();
+            case "chatEventType" -> Arrays.stream(ChatEventType.values()).map(e -> new EnumDto(e.name(), e.getDescription())).toList();
         };
     }
 
