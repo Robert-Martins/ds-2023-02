35b467d6f53068eabdb680624020cc89bd7d25d7
Robert-Martins <robertcantares@gmail.com>
2024-01-10 21:04:12 -0300

    feat/implement-send-message-method

--- swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/domain/services/MessageService.java
+++ swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/domain/services/MessageService.java
@@ -1,6 +1,10 @@
 package com.ufg.swift.chat.Swift.Chat.domain.services;
 
+import com.ufg.swift.chat.Swift.Chat.domain.models.Message;
 import com.ufg.swift.chat.Swift.Chat.domain.repositories.MessageRepository;
+import com.ufg.swift.chat.Swift.Chat.presentation.dtos.ChatEventDto;
+import com.ufg.swift.chat.Swift.Chat.presentation.dtos.ChatRoomUserDto;
+import com.ufg.swift.chat.Swift.Chat.presentation.services.IChatService;
 import com.ufg.swift.chat.Swift.Chat.presentation.services.IMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +14,25 @@ import org.springframework.stereotype.Service;
 public class MessageService implements IMessageService {
 
     private final MessageRepository messageRepository;
+
+    private final IChatService chatService;
+
+    @Override
+    public void send(String message, ChatRoomUserDto chatRoomUserDto) {
+        Message newMessage = new Message();
+        newMessage.setMessage(message);
+        newMessage.setAuthor(
+                chatRoomUserDto
+                        .mapDtoToEntity()
+                        .getUser()
+        );
+        Message savedMessage = this.messageRepository.save(newMessage);
+        this.chatService.publishEvent(
+                ChatEventDto.buildMessageEvent(
+                        chatRoomUserDto.getChatRoom().getId(),
+                        savedMessage.mapEntityToDto()
+                )
+        );
+    }
 
 }
