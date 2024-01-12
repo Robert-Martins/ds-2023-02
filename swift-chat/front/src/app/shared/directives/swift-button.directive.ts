bcccb95c35c116753d266292c5f873192d7aa148
Robert-Martins <robertcantares@gmail.com>
2024-01-12 20:38:24 -0300

    feat/button-directive

--- swift-chat/front/src/app/shared/directives/swift-button.directive.ts
+++ swift-chat/front/src/app/shared/directives/swift-button.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appSwiftButton]'
+  selector: '[swiftButton]'
 })
 export class SwiftButtonDirective {
 
-  constructor() { }
+  @Input()
+  public swiftButtonType: 'flat' | 'icon' = 'flat';
+
+  @Input()
+  public swiftButtonGhost: boolean = false;
+
+  @Input()
+  public swiftButtonIcon?: string = '';
+
+  constructor(
+    private renderer: Renderer2,
+    private elementRef: ElementRef
+  ) { 
+    this.renderer.addClass(this.elementRef, this.buildClasses());
+  }
+
+  private buildClasses(): string {
+    return `swift-button ${this.swiftButtonType} ${this.swiftButtonType === 'icon' ? this.swiftButtonIcon : ''} ${this.swiftButtonGhost ? 'ghost' : ''}`;
+  }
 
 }
