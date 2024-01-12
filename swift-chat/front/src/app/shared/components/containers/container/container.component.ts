19d6ad5ea794cc428838f6243c2630638819b856
Robert-Martins <robertcantares@gmail.com>
2024-01-12 20:19:04 -0300

    feat/container-module-and-centered-container-component

---
+++ swift-chat/front/src/app/shared/components/containers/container/container.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'swift-container',
+  template: `
+    <div class="swift-container">
+      <ng-content></ng-content>
+    </div>
+  `,
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+
+}
---
+++ swift-chat/front/src/app/shared/components/containers/containers.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from './container/container.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContainerComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+   exports: [
+    ContainerComponent
+   ]
+})
+export class ContainersModule { }
