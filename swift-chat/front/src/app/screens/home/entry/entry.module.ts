a7f8459df51993b84d8b679f06df40a78cab1d9f
Robert-Martins <robertcantares@gmail.com>
2024-01-12 20:01:27 -0300

    feat/remove-unnecessary-component

--- swift-chat/front/src/app/screens/home/entry/entry.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { EntryComponent } from './entry.component';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, EntryComponent],
-})
-export class EntryModule {}
