0967c2e011f61b46620ef750f602bc82165553ff
Robert-Martins <robertcantares@gmail.com>
2024-01-13 01:46:24 -0300

    feat/code-insert-tab

--- swift-chat/front/src/app/screens/code/code.component.ts
+++ swift-chat/front/src/app/screens/code/code.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { ChatRoomService } from '../../core/services/chat-room.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-code',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './code.component.html',
-  styleUrl: './code.component.css',
+  styleUrl: './code.component.css'
 })
-export class CodeComponent {}
+export class CodeComponent {
+
+  constructor(
+    private chatRoomService: ChatRoomService,
+    private router: Router
+  ) {
+
+  }
+
+  public onCodeComplete(code: string): void {
+    this.chatRoomService.getIdByCode(code)
+      .subscribe(
+        {
+          next: (id: string) => {
+            this.router.navigate([`lobby/${id}`]);
+          },
+          error: (error) => {
+            
+          }
+        }
+      );
+  }
+
+}
