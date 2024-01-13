7deaddf3aaf020ed6e5d76eba1bcad55a3fc9351
Yuan <108840666+YuanCalixto@users.noreply.github.com>
2024-01-13 00:24:51 +0000

    fix/chat-layout

--- swift-chat/front/src/app/screens/home/home.component.ts
+++ swift-chat/front/src/app/screens/home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterOutlet],
+  imports: [CommonModule, FormsModule, RouterOutlet, SharedModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
