df7133bd832bac94f9121073b81925f4ce3d0536
Robert-Martins <robertcantares@gmail.com>
2024-01-13 01:53:55 -0300

    feat/define-app-splash-screen-and-snackbar

---
+++ swift-chat/front/src/app/shared/components/splash-screen/splash-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrl: './splash-screen.component.css'
+})
+export class SplashScreenComponent {
+
+}
