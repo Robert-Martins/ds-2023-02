c24870f1c012d19b72acffbda113de289ab94f4a
Robert-Martins <robertcantares@gmail.com>
2024-01-14 12:21:26 -0300

    feat/implement-share-chat-dialog

--- swift-chat/front/src/app/shared/components/dialogs/share-chat-dialog/share-chat-dialog.component.ts
+++ swift-chat/front/src/app/shared/components/dialogs/share-chat-dialog/share-chat-dialog.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { SnackbarService } from '../../snackbar/snackbar.service';
 
 @Component({
-  selector: 'app-share-chat-dialog',
+  selector: 'swift-share-chat-dialog',
   templateUrl: './share-chat-dialog.component.html',
   styleUrl: './share-chat-dialog.component.css'
 })
 export class ShareChatDialogComponent {
+
+  public chatRoomUrl$: BehaviorSubject<string> = new BehaviorSubject(null);
+
+  public chatRoomCode$: BehaviorSubject<string> = new BehaviorSubject(null);
+
+  public qrCodeUrl$: BehaviorSubject<string> = new BehaviorSubject(null);
+
+  private readonly COPY_TO_CLIPBOARD_MESSAGE: string = "Copiado para a área de transferência";
+
+  @Input()
+  public set chatRoomId(id: string) {
+    this.chatRoomUrl$.next(this.buildChatRoomUrl(id));
+  }
+
+  @Input()
+  public set chatRoomCode(code: string) {
+    this.chatRoomCode$.next(code);
+  }
+
+  constructor(
+    private router: Router,
+    private snack: SnackbarService
+  ) {
+  }
+
+  public onCopy(): void {
+    this.snack.info(this.COPY_TO_CLIPBOARD_MESSAGE);
+  }
+
+  private buildChatRoomUrl(id: string): string {
+    return `${this.router.url.slice(0, this.router.url.lastIndexOf('/'))}/lobby/${id}`;
+  }
 
 }
