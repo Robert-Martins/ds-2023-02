2132e977677fdba2231f4e217b2f388d5b24386b
Robert-Martins <robertcantares@gmail.com>
2024-01-14 12:22:55 -0300

    feat/qr-code-component-implement

--- swift-chat/front/src/app/shared/components/dialogs/share-chat-dialog/qrcode/qrcode.component.ts
+++ swift-chat/front/src/app/shared/components/dialogs/share-chat-dialog/qrcode/qrcode.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { environment } from '../../../../../../environments/environment';
 
 @Component({
   selector: 'swift-qrcode',
@@ -6,6 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './qrcode.component.css'
 })
 export class QrcodeComponent {
-  
+
+  public qrCode$: BehaviorSubject<string> = new BehaviorSubject(null);
+
+  private readonly DEFAULT_QR_CODE_SIZE: string = 'size=320x320';
+
+  private readonly DEFAULT_QR_CODE_COLORS: string = 'color=5F3DC4&bgcolor=000';
+
+  @Input()
+  public url$: BehaviorSubject<string> = new BehaviorSubject(null);
+
+  public onClickClose(): void {
+    this.qrCode$.next(null);
+  }
+
+  constructor() {
+    this.url$.subscribe(this.setQrCodeUrl);
+  }
+
+  private setQrCodeUrl = (url: string): void => {
+    this.qrCode$.next(this.buildQrCodeUrl(url));
+  }
+
+  private buildQrCodeUrl(url: string): string {
+    return `${environment.qrCodeApi}?${this.DEFAULT_QR_CODE_SIZE}&${this.DEFAULT_QR_CODE_COLORS}&data=${url}`;
+  }
 
 }
--- swift-chat/front/src/environments/environment.ts
+++ swift-chat/front/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     apiUrl: 'http://localhost:9090',
-    qrCodeApi: 'https://api.qrserver.com/v1/create-qr-code', // API docs https://goqr.me/api/
+    qrCodeApi: 'https://api.qrserver.com/v1/create-qr-code/', // API docs https://goqr.me/api/
 }
