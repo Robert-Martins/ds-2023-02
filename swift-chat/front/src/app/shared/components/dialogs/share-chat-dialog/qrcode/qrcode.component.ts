c8470e0b58e5214cbb4ce896fa7dc268aab6fd77
Robert-Martins <robertcantares@gmail.com>
2024-01-14 00:15:33 -0300

    feat/chat-room-share-dialog

---
+++ swift-chat/front/src/app/shared/components/dialogs/share-chat-dialog/qrcode/qrcode.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'swift-qrcode',
+  templateUrl: './qrcode.component.html',
+  styleUrl: './qrcode.component.css'
+})
+export class QrcodeComponent {
+  
+
+}
---
+++ swift-chat/front/src/app/shared/components/dialogs/share-chat-dialog/share-chat-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-share-chat-dialog',
+  templateUrl: './share-chat-dialog.component.html',
+  styleUrl: './share-chat-dialog.component.css'
+})
+export class ShareChatDialogComponent {
+
+}
