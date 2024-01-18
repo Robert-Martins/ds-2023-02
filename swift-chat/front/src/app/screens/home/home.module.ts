b40d6848cb77ba3c0ec9f16159e3ce7f56b5a148
Robert-Martins <robertcantares@gmail.com>
2024-01-17 21:26:16 -0300

    feat/remove-unnecessary-modules

--- swift-chat/front/src/app/screens/home/home.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { HomeComponent } from './home.component';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, HomeComponent],
-})
-export class HomeModule {}
