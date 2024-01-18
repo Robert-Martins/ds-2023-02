8e71e1e231e79425bcb37b1f5b06c648e11fb658
Robert-Martins <robertcantares@gmail.com>
2024-01-17 21:16:28 -0300

    feat/app-home

--- swift-chat/front/src/app/screens/home/home.component.ts
+++ swift-chat/front/src/app/screens/home/home.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterOutlet, SharedModule],
+  imports: [CommonModule, FormsModule, RouterOutlet, RouterModule, SharedModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
