641ae1ff13252bf6967c02ecce01f1fedfb3c777
Robert-Martins <robertcantares@gmail.com>
2024-01-17 21:25:32 -0300

    feat/define-and-include-room-component

--- swift-chat/front/src/app/app.routes.ts
+++ swift-chat/front/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ChatComponent } from './screens/chat/chat.component';
 import { CodeComponent } from './screens/code/code.component';
 import { HomeComponent } from './screens/home/home.component';
 import { LobbyComponent } from './screens/lobby/lobby.component';
+import { RoomComponent } from './screens/room/room.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'code', component: CodeComponent },
+    { path: 'room', component: RoomComponent },
     { path: 'lobby/:id', component: LobbyComponent },
     { path: 'chat/:id', component: ChatComponent },
     { path: '**', redirectTo: '' }
