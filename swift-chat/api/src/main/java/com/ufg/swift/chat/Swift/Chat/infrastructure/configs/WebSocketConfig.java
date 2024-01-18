66d921fe2364665d00c0b0255d634950f5a4767f
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2024-01-18 20:39:47 -0300

    feat/web-socket-messaging-templae

--- swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/infrastructure/configs/WebSocketConfig.java
+++ swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/infrastructure/configs/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.ufg.swift.chat.Swift.Chat.infrastructure.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ExecutorSubscribableChannel;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
@@ -14,9 +17,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
+    public void configureMessageBroker(MessageBrokerRegistry config) {
         config.setApplicationDestinationPrefixes("/app");
         config.enableSimpleBroker("/room/{chatId}");
     }
 
+    @Bean
+    public SimpMessagingTemplate messagingTemplate() {
+        return new SimpMessagingTemplate(chatMessageChannel());
+    }
+
+    @Bean
+    public ExecutorSubscribableChannel chatMessageChannel() {
+        return new ExecutorSubscribableChannel();
+    }
 }
