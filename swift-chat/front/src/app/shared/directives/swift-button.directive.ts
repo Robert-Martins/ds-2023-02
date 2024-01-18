03ddaaee3e630e240ecab92a22859f6250a3d2f5
Robert-Martins <robertcantares@gmail.com>
2024-01-17 21:15:29 -0300

    feat/btns-styles-and-directive-enhance

--- swift-chat/front/src/app/shared/directives/swift-button.directive.ts
+++ swift-chat/front/src/app/shared/directives/swift-button.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2, booleanAttribute } from '@angular/core';
 
 @Directive({
   selector: '[swiftButton]'
@@ -6,10 +6,7 @@ import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 export class SwiftButtonDirective {
 
   @Input()
-  public swiftButtonType: 'flat' | 'icon' = 'flat';
-
-  @Input()
-  public swiftButtonGhost: boolean = false;
+  public swiftButtonType: 'flat' | 'flat-ghost' | 'icon' = 'flat';
 
   @Input()
   public swiftButtonIcon?: string = '';
@@ -18,11 +15,12 @@ export class SwiftButtonDirective {
     private renderer: Renderer2,
     private elementRef: ElementRef
   ) { 
+    console.log(this.swiftButtonType)
     this.renderer.setAttribute(this.elementRef.nativeElement, 'class', this.buildClasses().trim());
   }
 
   private buildClasses(): string {
-    return `swift-button ${this.swiftButtonType} ${this.swiftButtonType === 'icon' ? this.swiftButtonIcon : ''} ${this.swiftButtonGhost ? 'ghost' : ''}`;
+    return `swift-button ${this.swiftButtonType.split('-').join(' ')} ${this.swiftButtonType === 'icon' ? this.swiftButtonIcon : ''}`;
   }
 
 }
