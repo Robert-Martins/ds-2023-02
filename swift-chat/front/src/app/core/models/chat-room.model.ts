7d5606e6d5e63ec63abf59c60fd77274895fc6b6
Robert-Martins <robertcantares@gmail.com>
2024-01-18 22:06:42 -0300

    feat/lobby-component

--- swift-chat/front/src/app/core/models/chat-room.model.ts
+++ swift-chat/front/src/app/core/models/chat-room.model.ts
@@ -1,3 +1,4 @@
+import { ChatRoomUser } from "./chat-room-user.model";
 import { Message } from "./message.model";
 
 export class ChatRoom {
@@ -5,17 +6,20 @@ export class ChatRoom {
     public id: string;
     public name: string;
     public messages: Message[] = [];
+    public chatRoomUsers: ChatRoomUser[] = [];
     public createdAt: Date;
 
     constructor(
         id: string = null,
         name: string = null,
         messages: Message[] = [],
+        chatRoomUsers: ChatRoomUser[] = [],
         createdAt: Date = null
     ) {
         this.id = id;
         this.name = name;
         this.messages = messages;
+        this.chatRoomUsers = chatRoomUsers;
         this.createdAt = createdAt;
     }
 
--- swift-chat/front/src/app/screens/lobby/lobby.component.ts
+++ swift-chat/front/src/app/screens/lobby/lobby.component.ts
@@ -1,19 +1,84 @@
-import { Component, Injector } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Injector } from '@angular/core';
 import { UtilComponent } from '../../shared/components/util/util.component';
+import { SharedModule } from '../../shared/shared.module';
+import { ChatRoomService } from '../../core/services/chat-room.service';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ChatRoom } from '../../core/models/chat-room.model';
+import { BehaviorSubject } from 'rxjs';
+import { ChatRoomUserService } from '../../core/services/chat-room-user.service';
+import { ChatService } from '../../core/services/chat.service';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-lobby',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './lobby.component.html',
   styleUrl: './lobby.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LobbyComponent extends UtilComponent {
 
+  public chatRoom$: BehaviorSubject<ChatRoom> = new BehaviorSubject(null);
+
   constructor(
-    private injector: Injector
+    private chatRoomService: ChatRoomService,
+    private chatRoomUserService: ChatRoomUserService,
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+    injector: Injector
   ) {
     super(injector);
+    this.getRouteData();
+  }
+
+  public join(): void {
+    this.loading.start();
+    const chatRoomId: string = this.chatRoom$.value?.id;
+    const userId: string = this.userService.getUserId();
+    this.chatRoomUserService.join(
+      chatRoomId,
+      userId
+    ).subscribe({
+      next: () => {
+        this.router.navigate([`/chat/${chatRoomId}`]);
+      },
+      error: error => {
+        this.chatRoom$.next(null);
+        this.snack.error(error?.message);
+        this.snack.info('Você será redirecionado para a Home');
+        this.loading.stop();
+        this.router.navigate(['']);
+      }
+    });
+  }
+
+  private getRouteData(): void {
+    this.loading.start();
+    this.route.paramMap.subscribe({
+      next: (map: ParamMap) => {
+        this.findChatRoomById(map.get('id'));
+      },
+      error: () => {
+        this.snack.error("Acesso inválido");
+      }
+    });
+  }
+
+  private findChatRoomById(id: string): void {
+    this.chatRoomService.read(id)
+      .subscribe({
+        next: (chatRoom: ChatRoom) => {
+          this.chatRoomService.persistChatRoomId(chatRoom?.id);
+          this.chatRoom$.next(chatRoom);
+          this.loading.stop();
+        },
+        error: error => {
+          this.snack.error(error?.message);
+          this.loading.stop();
+        }
+      });
   }
   
 }
