7340977de9973eb58d07b6e4021ab59509010a50
Robert-Martins <robertcantares@gmail.com>
2024-01-18 21:29:12 -0300

    feat/tooltip-finished

--- swift-chat/front/src/app/shared/directives/directives.module.ts
+++ swift-chat/front/src/app/shared/directives/directives.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SwiftButtonDirective } from './swift-button.directive';
 import { CopyToClipboardDirective } from './copy-to-clipboard.directive';
+import { TooltipDirective } from './tooltip.directive';
 
 
 
 @NgModule({
   declarations: [
     SwiftButtonDirective,
-    CopyToClipboardDirective
+    CopyToClipboardDirective,
+    TooltipDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SwiftButtonDirective,
-    CopyToClipboardDirective
+    CopyToClipboardDirective,
+    TooltipDirective
   ]
 })
 export class DirectivesModule { }
