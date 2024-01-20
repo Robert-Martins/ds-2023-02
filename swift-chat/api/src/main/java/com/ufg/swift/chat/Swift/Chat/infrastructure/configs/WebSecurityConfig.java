53a97b3940eeb3fc613063b2e8f982c043240e8f
Robert-Martins <robertcantares@gmail.com>
2024-01-19 22:16:24 -0300

    feat/cors-config-update

--- swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/infrastructure/configs/WebSecurityConfig.java
+++ swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/infrastructure/configs/WebSecurityConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,6 +19,13 @@ public class WebSecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 
 }
