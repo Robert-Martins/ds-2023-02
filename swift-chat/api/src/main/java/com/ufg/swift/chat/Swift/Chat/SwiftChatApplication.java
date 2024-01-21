8136468ade46b43678ac90716367ae6b7a34cf3e
Robert-Martins <robertcantares@gmail.com>
2024-01-20 23:40:18 -0300

    feat/websocket-allowed-origin

--- swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/SwiftChatApplication.java
+++ swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/SwiftChatApplication.java
@@ -2,8 +2,12 @@ package com.ufg.swift.chat.Swift.Chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @SpringBootApplication
+@EnableWebSocketMessageBroker
+@EnableWebSocket
 public class SwiftChatApplication {
 
 	public static void main(String[] args) {
--- swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/infrastructure/configs/WebSocketConfig.java
+++ swift-chat/api/src/main/java/com/ufg/swift/chat/Swift/Chat/infrastructure/configs/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:4200").withSockJS();
     }
 
     @Override
